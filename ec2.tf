7d68f60b544aeb56b66a1854209c084ca74fe6a6
Hitman1245 <rohit.kamble@blazeclan.com>
2022-03-09 13:49:50 +0530

    Frist pipeline code

---
+++ ec2.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "mysg" {
+  name        = "mydemo"
+  description = "Allow TLS inbound traffic"
+
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "My Security Group"
+  }
+}
+
+
+resource "aws_instance" "my-ec2" {
+  ami               = var.ami
+  instance_type     = var.instance_type
+  availability_zone = "ap-south-1a"
+  security_groups   = ["aws_security_group.mysg"]
+}
---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "ami" {
+    default = "ami-03fa4afc89e4a8a09"
+}
+
